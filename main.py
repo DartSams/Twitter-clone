3ace7242fd8058ae4fa2ff2a15b410b2119538cc
dartagnan sams <dartagnansams1@icloud.com>
2021-08-25 16:13:54 -0400

    removed mysql

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ bcrypt = Bcrypt(app)  ##to encrypt passwd https://flask-bcrypt.readthedocs.io/en
 app.config["SECRET_KEY"] = "hello"  # use session to save personal data to so user doesnt have to log in over and over
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-mysql = MySQL()  # to connect flask to mysql
-app.config["MYSQL_DATABASE_HOST"] = "us-cdbr-east-04.cleardb.com"
-app.config["MYSQL_DATABASE_USER"] = "bcc2ec4fcecbe5"
-app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("password")
-app.config["MYSQL_DATABASE_DB"] = "heroku_d10e4ce632a9633"
-mysql.init_app(app)  # init the flask  to mysql connection
-conn = mysql.connect()
-mycursor = conn.cursor()
+# mysql = MySQL()  # to connect flask to mysql
+# app.config["MYSQL_DATABASE_HOST"] = "us-cdbr-east-04.cleardb.com"
+# app.config["MYSQL_DATABASE_USER"] = "bcc2ec4fcecbe5"
+# app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("password")
+# app.config["MYSQL_DATABASE_DB"] = "heroku_d10e4ce632a9633"
+# mysql.init_app(app)  # init the flask  to mysql connection
+# conn = mysql.connect()
+# mycursor = conn.cursor()
 
 socketio = SocketIO(app)  # init the socket connection
 
